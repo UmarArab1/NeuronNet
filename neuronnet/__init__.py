80bc33cb053dea24cfc89a754df01dffdef61ce7
UmarArab1 <umarfrost2011@gmail.com>
2025-03-30 03:02:39 +0500

    Update __init__.py

--- neuronnet/__init__.py
+++ neuronnet/__init__.py
@@ -1,2 +1,10 @@
-from .model import CollectiveLearningModel
-from .training import collective_training
+import os
+import importlib
+
+# Получаем список всех файлов в папке библиотеки
+modules = [f[:-3] for f in os.listdir(os.path.dirname(__file__)) if f.endswith(".py") and f != "__init__.py"]
+
+# Динамически импортируем их
+for module in modules:
+    imported_module = importlib.import_module(f".{module}", package=__name__)
+    globals().update({name: getattr(imported_module, name) for name in dir(imported_module) if not name.startswith("_")})
